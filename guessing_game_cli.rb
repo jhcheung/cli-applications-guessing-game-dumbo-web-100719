7892eb9ca503cb299f1ab70e299c892ca84a8d34
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-09-28 09:34:31 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,16 @@
 # Code your solution here!
+
+def random_num
+  rand(1..6)
+end 
+
+def user_num
+  
+end 
+
+
 def run_guessing_game
-  random_num = rand(1..6)
+  random_num
   user_num = gets
   
 end 
